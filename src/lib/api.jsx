b67c108f60136ff38abdaa8a3147d939158a6c94
PriteeAr0te <arotepritee111@gmail.com>
2025-06-02 20:25:39 +0530

    backend url added

--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000/api",
+    baseURL: import.meta.env.VITE_BASE_URL || "https://contact-manager-backend-rho.vercel.app/api",
     withCredentials: true
 });
 
