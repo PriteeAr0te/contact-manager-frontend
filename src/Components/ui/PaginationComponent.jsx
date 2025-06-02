3174fa2c16266aa81f98ba76dcb23142c68843cc
PriteeAr0te <arotepritee111@gmail.com>
2025-06-02 17:20:52 +0530

    Pagination added for contacts page

---
+++ src/Components/ui/PaginationComponent.jsx
@@ -0,0 +1,59 @@
+export default function PaginationComponent({ currentPage, totalPages, onPageChange }) {
+    const pages = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+    }
+
+    console.log("totalPages1", totalPages);
+    console.log("currentPage1", currentPage);
+
+    return (
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                <div>
+                    <p className="text-sm text-gray-700">
+                        Page <span className="font-medium">{currentPage}</span> of{' '}
+                        <span className="font-medium">{totalPages}</span>
+                    </p>
+                </div>
+                <div>
+                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+                        <button
+                            disabled={currentPage === 1}
+                            onClick={() => onPageChange(currentPage - 1)}
+                            className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed"
+                        >
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" /></svg>
+                            </span>
+                        </button>
+
+                        {pages.map((page) => (
+                            <button
+                                key={page}
+                                onClick={() => onPageChange(page)}
+                                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${page === currentPage
+                                    ? 'z-10 bg-indigo-600 text-white'
+                                    : 'text-gray-900'
+                                    }`}
+                            >
+                                {page}
+                            </button>
+                        ))}
+
+                        <button
+                            disabled={currentPage === totalPages}
+                            onClick={() => onPageChange(currentPage + 1)}
+                            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed"
+                        >
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" /></svg>
+                            </span>
+                        </button>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import API from '../lib/api';
 import EditContactModal from '../Components/ui/EditContactModal';
 import { Slide, toast, ToastContainer } from 'react-toastify';
 import DropdownComponent from '../Components/ui/DropdownComponent';
+import PaginationComponent from '../Components/ui/PaginationComponent';
 
 const Home = () => {
   const [contacts, setContacts] = useState([]);
@@ -13,6 +14,8 @@ const Home = () => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [selectedTag, setSelectedTag] = useState('');
   const [isFavorite, setIsFavorite] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleTagChange = (selectedOption) => {
     setSelectedTag(selectedOption);
@@ -20,16 +23,17 @@ const Home = () => {
 
   useEffect(() => {
     const delayBounce = setTimeout(() => {
-      fetchContacts();
+      fetchContacts(currentPage);
     }, 500);
     return () => clearTimeout(delayBounce);
-  }, [searchTerm, selectedTag, isFavorite]);
+  }, [searchTerm, selectedTag, isFavorite, currentPage]);
 
-  const fetchContacts = async () => {
+  const fetchContacts = async (page = 1) => {
     try {
       const token = localStorage.getItem('token');
 
-      const params = {};
+      const params = { page, limit: 5 };
+
       if (searchTerm) params.search = searchTerm;
       if (selectedTag) params.tag = selectedTag;
       if (isFavorite) params.isFavorite = true;
@@ -38,15 +42,13 @@ const Home = () => {
         headers: {
           Authorization: `Bearer ${token}`
         },
-        // params: {
-        //   search: searchTerm,
-        //   tag: selectedTag ? selectedTag.value : '',
-        //   isFavorite: isFavorite ? true : false
-        // }
         params
       });
       const data = response.data;
       setContacts(data);
+      setTotalPages(data.totalPages);
+      setCurrentPage(data.page);
+      console.log("totalpages in fetchContacts:", totalPages);
 
       if (searchTerm.length > 0) {
         const names = data.map((contact) => contact.name);
@@ -112,6 +114,8 @@ const Home = () => {
     }
   }
 
+  console.log("contacts:", contacts);
+
   return (
     <>
       <EditContactModal isOpen={isOpenEdit} setIsOpen={setIsOpenEdit} contact={selectedContact} />
@@ -144,7 +148,7 @@ const Home = () => {
               )}
             </button>
 
-            <div className="flex items-center rounded-md bg-transparent pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600 relative">
+            <div className="flex items-center rounded-md bg-transparent pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-1 has-[input:focus-within]:-outline-offset-1 has-[input:focus-within]:outline-indigo-600 relative">
               <div className="shrink-0 text-base text-gray-500 dark:text-gray select-none sm:text-sm/6">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentcolor"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg>
               </div>
@@ -201,11 +205,11 @@ const Home = () => {
           </div>
         </div>
 
-        {contacts.length === 0 ? (
+        {contacts.contacts?.length === 0 ? (
           <p className="text-center text-gray-500 dark:text-gray-200">No contacts available.</p>
         ) : (
           <ul className="space-y-4">
-            {contacts.map((contact) => (
+            {contacts?.contacts?.map((contact) => (
               <li key={contact._id} className="border border-light dark:border-gray-100 rounded-lg p-4 shadow-sm flex sm:justify-between justify-center sm:flex-row flex-col gap-y-4 items-center">
                 <div>
                   <p className="font-semibold text-gray-800 text-lg dark:text-gray-100 capitalize">{contact.name}</p>
@@ -231,6 +235,16 @@ const Home = () => {
           </ul>
         )}
       </div>
+
+      <div>
+        {console.log("pages: ", totalPages, "current page: ", currentPage)}
+        <PaginationComponent
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      </div>
+
     </>
   );
 }
