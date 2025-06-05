615be163b368466fffb09311ae0a6a794b6a4b9f
PriteeAr0te <arotepritee111@gmail.com>
2025-06-05 18:58:54 +0530

    dark mode stored in localstorage

---
+++ src/Hookes/useDarkMode.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useState(false)
+
+    useEffect(() => {
+        const setTheme = localStorage.getItem('darkMode');
+        const isDark = setTheme === 'true';
+        setDarkMode(isDark);
+        console.log("dark: ", isDark)
+        document.documentElement.classList.toggle('dark', isDark)
+    }, []);
+
+    const toggleDarkMode = () => {
+        const newMode = !darkMode;
+        setDarkMode(newMode);
+        localStorage.setItem('darkMode', newMode);
+        console.log(newMode)
+        document.documentElement.classList.toggle('dark', newMode)
+    };
+
+  return [darkMode, toggleDarkMode]
+}
+
+export default useDarkMode
